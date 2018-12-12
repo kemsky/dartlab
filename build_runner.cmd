d237149c6c40b4542cdbfc641a576d26cf9743a0
Alexander Turtsevich <alexander.turtsevich@itmint.ca>
2018-12-12 21:33:31 +0300

    refactor

--- build_runner.cmd
+++ build_runner.cmd
@@ -1,4 +1,5 @@
 @echo off
 REM Invoke code generation
-REM Sometimes need to clean "flutter packages pub run build_runner clean"
+REM Clean: "flutter packages pub run build_runner clean"
+REM Watch mode: "flutter packages pub run build_runner watch --delete-conflicting-outputs"
 flutter packages pub run build_runner build --delete-conflicting-outputs
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:dart_lab/components/application.dart';
-import 'package:dart_lab/database/database_client.dart';
-import 'package:dart_lab/database/database_manager.dart';
-import 'package:dart_lab/database/database_repository.dart';
-import 'package:dart_lab/database/model/application_user.dart';
-import 'package:dart_lab/database/user_repository.dart';
 import 'package:dart_lab/middleware/middleware.dart';
 import 'package:dart_lab/state/actions.dart';
 import 'package:dart_lab/state/reducers.dart';
@@ -12,8 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:redux/redux.dart';
 import 'main.reflectable.dart';
+import 'package:reflectable/reflectable.dart';
 
 //todo: Add Error Reporting https://flutter.io/docs/cookbook/maintenance/error-reporting
+
+final Logger _logger = new Logger('main');
 
 void main(List<String> arguments) {
   Logger.root.level = Level.ALL;
@@ -21,44 +19,24 @@ void main(List<String> arguments) {
     print('${rec.level.name}: [${rec.loggerName}]: ${rec.message}');
   });
 
+  _logger.info("Starting application...");
+
   initializeReflectable();
 
   final dev_host = arguments.length == 2 ? arguments[0] : null;
   final dev_token = arguments.length == 2 ? arguments[1] : null;
 
-  //create store
+  if (arguments.length > 0) {
+    _logger.info("Development mode: host='$dev_host', token='$dev_token'");
+  }
+
   final store = Store<AppState>(
       appReducer,
       initialState: AppState.initial(dev_host, dev_token),
       middleware: createAppStateMiddleware());
 
-  //start application
   runApp(ApplicationStoreProvider(store));
 
   store.dispatch(loadPackageInfoAction);
   store.dispatch(getCurrentUserAction);
-
-  var databaseClient = DatabaseClient(DatabaseService());
-
-  var repository = DatabaseRepository(databaseClient);
-
-  UserRepository(repository).save(ApplicationUser((b) {
-    return b
-      ..token = 'token'
-      ..url = 'https://gitlab.com';
-  })).listen((_) {
-    print('insert data');
-  }, onError: (e) {
-    logger.info('insert error', e);
-  }, onDone: () {
-    logger.info('insert done');
-  });
-
-  UserRepository(repository).get().listen((data) {
-    print('tx data $data');
-  }, onDone: () {
-    logger.info('tx done');
-  }, onError: (e) {
-    logger.info('tx error', e);
-  });
 }
