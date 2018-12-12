a5fc23ba9559c858288977957a1e0d8edef9650a
Alexander Turtsevich <alexander.turtsevich@itmint.ca>
2018-12-12 12:57:14 +0300

    save key/value

--- lib/database/repository.dart
+++ lib/database/repository.dart
@@ -6,6 +6,15 @@ import 'package:reflectable/reflectable.dart';
 import 'package:rxdart/rxdart.dart';
 
 class KeyValueRepository {
+  static final Map<String, bool> IgnoredProperties = {
+    'hashCode': true,
+    '==': true,
+    'noSuchMethod': true,
+    'toString': true,
+    'runtimeType': true,
+    'serializer': true,
+  };
+
   final Logger logger = new Logger('KeyValueRepository');
 
   final DatabaseClient _client;
@@ -14,8 +23,8 @@ class KeyValueRepository {
 
   Observable<Map<String, dynamic>> load<T>(Type entityType) {
     ClassMirror classMirror = reflector.reflectType(entityType);
-
-    var selects = classMirror.instanceMembers.keys.map((key) => "SELECT key, value from KeyValue WHERE key = '$key'");
+    var prefix = classMirror.simpleName;
+    var selects = getKeys(classMirror).map((key) => "SELECT SUBSTR(key, LENGTH('${prefix}_') + 1) as key, value from KeyValue WHERE key = '${prefix}_${key}'");
     var statement = selects.join(' UNION ALL ');
 
     logger.info('SQL: $statement');
@@ -30,11 +39,9 @@ class KeyValueRepository {
   }
 
   Observable<void> save<T>(T entity) {
-
     Map<String, dynamic> values = model_serializers.serialize(entity);
 
-    //remove type name key
-    values.remove('\$');
+    String prefix = values.remove('\$');
 
     var statement = 'INSERT OR REPLACE INTO KeyValue (key, value) VALUES ';
     statement += List.generate(values.length, (index) => index).map((i) => ' (?, ?)').join(', ');
@@ -42,23 +49,29 @@ class KeyValueRepository {
     var params = [];
 
     values.forEach((key, value) {
-      params.add(key);
+      params.add('${prefix}_${key}');
       params.add(value);
     });
 
-    logger.info('SQL: $statement');
+    logger.info('SQL: $statement, params: $params');
 
     return this._client.query(statement, params);
   }
 
   Observable<void> delete<T>(Type entityType) {
     ClassMirror classMirror = reflector.reflectType(entityType);
-
-    var where = classMirror.instanceMembers.keys.map((key) => "key = '$key'");
+    var prefix = classMirror.simpleName;
+    var where = classMirror.instanceMembers.keys.map((key) => "key = '${prefix}_${key}'");
     var statement = 'DELETE FROM KeyValue WHERE ' + where.join(' OR ');
 
     logger.info('SQL: $statement');
 
     return this._client.query(statement);
+  }
+
+  Iterable<String> getKeys(ClassMirror classMirror) {
+    return classMirror.declarations.keys.where((key) {
+      return key != classMirror.simpleName && !IgnoredProperties.containsKey(key);
+    });
   }
 }
