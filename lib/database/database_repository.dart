993558467771f7bf1673a4b0e8750d52a2a1f514
Alexander Turtsevich <alexander.turtsevich@itmint.ca>
2018-12-12 13:45:48 +0300

    save key/value

---
+++ lib/database/database_repository.dart
@@ -0,0 +1,59 @@
+library database_repository;
+
+import 'dart:convert';
+
+import 'package:built_value/serializer.dart';
+import 'package:dart_lab/database/database_client.dart';
+import 'package:dart_lab/database/model/database_serializers.dart';
+import 'package:logging/logging.dart';
+import 'package:quiver/core.dart';
+import 'package:rxdart/rxdart.dart';
+
+class DatabaseRepository {
+  final Logger logger = new Logger('KeyValueRepository');
+
+  final DatabaseClient _client;
+
+  DatabaseRepository(this._client);
+
+  Observable<Optional<T>> load<T>(String key, Type entityType) {
+    final statement = "SELECT key, value from KeyValue WHERE key = ?";
+
+    final params = [key];
+
+    logger.info('SQL: $statement, params: $params');
+
+    return this._client.query(statement, params).map((rows) {
+      if (rows.isEmpty) {
+        return Optional.absent();
+      }
+      String result = rows.first['value'];
+
+      return Optional.of(model_serializers.deserialize(json.decode(result), specifiedType: FullType(entityType)));
+    });
+  }
+
+  Observable<void> save<T>(String key, T entity) {
+    final Map<String, dynamic> values = model_serializers.serialize(entity);
+
+    values.remove('\$');
+
+    final statement = 'INSERT OR REPLACE INTO KeyValue (key, value) VALUES (?, ?)';
+
+    final params = [key, json.encode(values)];
+
+    logger.info('SQL: $statement, params: $params');
+
+    return this._client.query(statement, params);
+  }
+
+  Observable<void> delete<T>(String key) {
+    final statement = 'DELETE FROM KeyValue WHERE key = ?';
+
+    final params = [key];
+
+    logger.info('SQL: $statement');
+
+    return this._client.query(statement, params);
+  }
+}
--- lib/database/user_repository.dart
+++ lib/database/user_repository.dart
@@ -1,12 +1,13 @@
 import 'package:dart_lab/database/database_client.dart';
-import 'package:dart_lab/database/model/database_serializers.dart';
 import 'package:dart_lab/database/model/application_user.dart';
-import 'package:dart_lab/database/repository.dart';
+import 'package:dart_lab/database/database_repository.dart';
 import 'package:logging/logging.dart';
 import 'package:quiver/core.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UserRepository {
+  static final String Key = 'ApplicationUser';
+
   final Logger logger = new Logger('UserRepository');
 
   final DatabaseClient _client;
@@ -14,21 +15,14 @@ class UserRepository {
   UserRepository(this._client);
 
   Observable<Optional<ApplicationUser>> get() {
-    return KeyValueRepository(this._client)
-        .load(ApplicationUser)
-        .map((map) {
-      if (map.isEmpty) {
-        return Optional.absent();
-      }
-      return Optional.of(model_serializers.deserializeWith(ApplicationUser.serializer, map));
-    });
+    return DatabaseRepository(this._client).load(Key, ApplicationUser);
   }
 
   Observable<void> save(ApplicationUser user) {
-    return KeyValueRepository(this._client).save(user);
+    return DatabaseRepository(this._client).save(Key, user);
   }
 
   Observable<void> remove() {
-    return KeyValueRepository(this._client).delete(ApplicationUser);
+    return DatabaseRepository(this._client).delete(Key);
   }
 }
